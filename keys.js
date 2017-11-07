bebe7b68911ac743bd341cfc8ec074f2eaf312b4
Kumari Priya <priya@goel.pw>
2017-11-06 22:29:39 -0800

    add keys for api call

---
+++ keys.js
@@ -0,0 +1,15 @@
+exports.omdbKeys = {
+  omdbKey: '40e9cece'
+};
+
+exports.twitterKeys = {
+  consumer_key: 'e9hNm9I46MEwSKluzovL1Iipk',
+  consumer_secret: 'GKlD6tOHqOwKI1EmWFEdHHREaaOd1NpjQwOo839ViXgWXwSoyY',
+  access_token_key: '916517636699123713-Q0aVR9WDlZRspnUQ5OkcIlqmYzxGiqp',
+  access_token_secret: 'IYhhucnmkZ1SQwVeBILjYibkyWSN34JGcGUicHf6ARSiJ'
+};
+
+exports.spotifykeys = {
+  id: '22bf2d47a39043038fc3ecf24f7cae72',
+  secret: '5c9a0d1d1ebe4a47932b404aab2a7e37'
+};
